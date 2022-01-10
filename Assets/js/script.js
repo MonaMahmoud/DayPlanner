92838e3954336f029ebc34dae30be98ac105139f
MonaMahmoud <monamohamedfahmy@gmail.com>
2022-01-10 18:08:13 +1100

    Check for previously stored tasks

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,6 @@
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 var currentMoment = moment();
+
 $("#timeBlocks tr").each(function () {
     if(($(this).attr("data-hour"))<currentMoment.hour()){
         $(this).addClass("past");
@@ -12,12 +13,25 @@ $("#timeBlocks tr").each(function () {
     }
 });
 
+
+$("#timeBlocks tr td input").each(function () {
+
+    storedTask = localStorage.getItem(($(this)).attr("id"));
+    //alert(storedTask);
+    if(storedTask!="undefined"){
+        //alert(($(this)).attr("id"));
+        ($(this)).val(storedTask) ;
+    }
+});
+
+
+
 $("button").on("click",function(){
     //console.log($(this).attr("data-hour")+"button");
     var selectedInput = $(this).attr("data-hour")+"input";
-    var selectedHour = $(this).attr("data-hour");
+    //var selectedHour = $(this).attr("data-hour");
     //console.log(selected);
     item = $('#' + selectedInput).val();
     //console.log($('#' + selected).val());
-    localStorage.setItem(selectedHour,item);
+    localStorage.setItem(selectedInput,item);
 })
